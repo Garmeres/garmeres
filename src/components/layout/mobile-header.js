485a017ad3bb0e1bee4806f805c70b327fdf561c
Levi Sørum <levi.sorum@gmail.com>
2021-03-21 12:23:30 +0100

    Added functionality to open and close mobile menu.

--- src/components/layout/mobile-header.js
+++ src/components/layout/mobile-header.js
@@ -3,16 +3,46 @@ import Social from "./social"
 import Logo from "./header-logo"
 import BurgerButton from "./burger-button"
 
+const MobileMenu = ({ menuData, lang, siblings, show }) => {
+  return (
+    <div id="mobile-menu-container">
+      <div
+        id="mobile-menu-overlay"
+        className={`${show != null ? (show ? "visible" : "hidden") : ""}`}
+      ></div>
+      <div
+        id="mobile-menu"
+        role="menu"
+        className={`${show != null ? (show ? "visible" : "hidden") : ""}`}
+      ></div>
+    </div>
+  )
+}
+
 const MobileHeader = ({ content, menuData, lang, siblings, logoImage }) => {
-  const [visibility, setVisibility] = useState("hidden")
+  const [showMenu, setShowMenu] = useState(null)
+
+  function toggleMenu() {
+    if (showMenu != null) {
+      setShowMenu(!showMenu)
+    } else setShowMenu(true)
+  }
 
   return (
     <div id="mobile-header-container">
-      <Logo image={logoImage} text={content.logo_text} />
-      <Social lang={lang} />
-      <div id="mobile-header-left">
-        <BurgerButton onClick={() => {}} />
+      <div id="mobile-header-content-container">
+        <Logo image={logoImage} text={content.logo_text} />
+        <Social lang={lang} />
+        <div id="mobile-header-left">
+          <BurgerButton onClick={() => toggleMenu()} />
+        </div>
       </div>
+      <MobileMenu
+        menuData={menuData}
+        lang={lang}
+        siblings={siblings}
+        show={showMenu}
+      />
     </div>
   )
 }
