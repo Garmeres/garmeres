29c63e6227b37538d400beb851f6f57c04593386
Levi Sørum <levi.sorum@gmail.com>
2021-05-06 21:45:02 +0200

    Updated social seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -8,12 +8,18 @@ function SEO({ description, lang, meta = [], title, content, image, url }) {
   return (
     <Helmet titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}>
       <html lang={lang === "default" ? "en" : "no"} />
-      <title>{title ? title : content.title}</title>
-      <meta name="title" content={title ? title : content.title} />
+      <title>{title != null && title !== "" ? title : content.title}</title>
+      <meta
+        name="title"
+        content={title != null && title !== "" ? title : content.title}
+      />
       <meta name="description" content={metaDescription} />
 
       <meta property="og:url" content={url} />
-      <meta property="og:title" content={title ? title : content.title} />
+      <meta
+        property="og:title"
+        content={title != null && title !== "" ? title : content.title}
+      />
       <meta property="og:description" content={metaDescription} />
 
       <meta property="og:type" content="website" />
@@ -27,7 +33,10 @@ function SEO({ description, lang, meta = [], title, content, image, url }) {
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={url} />
-      <meta property="twitter:title" content={title ? title : content.title} />
+      <meta
+        property="twitter:title"
+        content={title != null && title !== "" ? title : content.title}
+      />
       <meta property="twitter:description" content={metaDescription} />
 
       {meta.map(item => (
