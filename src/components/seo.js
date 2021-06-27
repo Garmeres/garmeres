0859e3f9232c964e86c091601e5e55019ca00af8
Levi Sørum <levi.sorum@gmail.com>
2021-06-27 14:05:01 +0200

    Added names to seo meta tags.

--- src/components/seo.js
+++ src/components/seo.js
@@ -24,20 +24,34 @@ function SEO({
       />
       <meta name="description" content={metaDescription} />
 
-      <meta property="og:url" content={url} />
+      <meta name="og:url" property="og:url" content={url} />
       <meta
+        name="og:title"
         property="og:title"
         content={title != null && title !== "" ? title : content.title}
       />
-      <meta property="og:description" content={metaDescription} />
+      <meta
+        name="og:description"
+        property="og:description"
+        content={metaDescription}
+      />
 
-      <meta property="og:type" content="website" />
-      <meta property="og:site_name" content={content.title} />
+      <meta name="og:type" property="og:type" content="website" />
+      <meta
+        name="og:site_name"
+        property="og:site_name"
+        content={content.title}
+      />
 
-      {image != null ? <meta property="og:image" content={image} /> : null}
-      {image != null ? <meta property="twitter:image" content={image} /> : null}
+      {image != null ? (
+        <meta name="og:image" property="og:image" content={image} />
+      ) : null}
+      {image != null ? (
+        <meta name="twitter:image" property="twitter:image" content={image} />
+      ) : null}
       {image != null ? (
         <meta
+          name="twitter:image:alt"
           property="twitter:image:alt"
           content={
             imageAlt != null && imageAlt !== ""
@@ -47,13 +61,18 @@ function SEO({
         />
       ) : null}
 
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={url} />
       <meta
+        name="twitter:card"
+        property="twitter:card"
+        content="summary_large_image"
+      />
+      <meta name="twitter:url" property="twitter:url" content={url} />
+      <meta
+        name="twitter:title"
         property="twitter:title"
         content={title != null && title !== "" ? title : content.title}
       />
-      <meta property="twitter:description" content={metaDescription} />
+      <meta name="twitter:description" property="twitter:description" content={metaDescription} />
 
       {meta.map(item => (
         <meta name={item.name} content={item.content} />
