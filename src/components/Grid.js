88548aac22b330f49904ede183251e36ba9ef5f3
Levi Sørum <levi.sorum@gmail.com>
2021-10-16 17:04:16 +0200

    Removed missing key error in Grid.

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,11 +5,13 @@ import "../style/grid.css"
 
 const Grid = ({ blok }) => {
   const itemWidth = 100 / blok.max_columns
+  var k = 0
   return (
     <SbEditable content={blok} key={blok._uid}>
       <ul className="grid-container">
         {blok.items.map(blok => (
           <li
+            key={k++}
             className={"grid-item " + blok.component}
             style={{
               minWidth: itemWidth + "%",
